4db84cf30a7205877cfab11ace965b156b9b968f
Rudy <enriquecollantes9@gmail.com>
2021-02-04 00:55:28 -0500

    Proyecto avanzado

---
+++ ProyectoWebApp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, HttpResponse
+
+from servicios.models import Servicio
+
+# Create your views here.
+
+def home(request):
+
+    return render(request, "ProyectoWebApp/home.html")
+
+
+
+
+def tienda(request):
+
+    return render(request, "ProyectoWebApp/tienda.html")
+
+
+
+
+
+  
